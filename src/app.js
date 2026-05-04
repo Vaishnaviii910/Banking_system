db87f5aa7078c4d1ce9e391dd9bd0e7cd3f57776
vaishnaviii910 <vaishnavisingh4956@gmail.com>
2026-05-05 04:05:48 +0530

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,22 @@
+const express = require("express")
+const cookieParser = require("cookie-parser")
+
+
+const app = express()
+
+app.use(express.json())
+app.use(cookieParser())
+
+/**
+ * -Routes required
+ */
+const authRouter = require("./routes/auth.routes")
+const accountRouter = require("./routes/account.routes")
+
+/**
+ * - Use Routes
+ */
+
+app.use("/api/auth", authRouter)
+app.use("/api/account",accountRouter)
+module.exports = app
